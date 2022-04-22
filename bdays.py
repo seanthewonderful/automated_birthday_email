4351add102f1ca7cfd9f09d14873a9589b7e6c42
Sean Fagan <58955260+seanthewonderful@users.noreply.github.com>
2022-04-21 21:21:24 -0600

    bdays to dictionary, search for tuple to match todays date, choose random template from template folder and replace text with name corresponding to date tuple if match was found

--- bdays.py
+++ bdays.py
@@ -1,21 +1,31 @@
 ##################### Extra Hard Starting Project ######################
 import pandas
 import datetime as dt
-
+import random
 
 # 1. Update the birthdays.csv
-data = pandas.read_csv("birthdays.csv")
-
-print(data.year)
 
 # 2. Check if today matches a birthday in the birthdays.csv
 now = dt.datetime.now()
-year = now.year
-month = now.month
-day = now.day
+today = (now.month, now.day)
+print(today)
 
-with open("birthdays.csv") as bday_data:
-    
+data = pandas.read_csv("birthdays.csv")
+bday_dict = {datarow["name"]:(datarow.month, datarow.day) for (index, datarow) in data.iterrows()}  
+# print(bday_dict)
+
+letter_list = ["letter_templates/letter_1.txt", "letter_templates/letter_2.txt", "letter_templates/letter_3.txt"]
+
+for (name, bday) in bday_dict.items():
+    if bday == today:
+        print(name)
+        letter = random.choice(letter_list)
+        with open(letter, 'r') as file :
+            letter_text = file.read()
+        letter_text = letter_text.replace("[NAME]", name)
+        print(letter_text)
+
+
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 
